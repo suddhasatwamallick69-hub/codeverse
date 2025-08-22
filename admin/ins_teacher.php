ab251762332040d791bd8dd478058a0582599ff4
SuddhasatwaMallick <suddhasatwamallick69@gmail.com>
2025-08-22 21:18:29 +0530

    Update ins_teacher.php

--- admin/ins_teacher.php
+++ admin/ins_teacher.php
@@ -31,7 +31,7 @@ use PHPMailer\PHPMailer\Exception;
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'suddhasatwamallick7@gmail.com';                     //SMTP username
-        $mail->Password   = 'ilte nibl bzvh towt';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
@@ -68,4 +68,5 @@ use PHPMailer\PHPMailer\Exception;
         </script>";
 
     }
+
 ?>
